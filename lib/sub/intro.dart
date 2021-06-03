6de4c4a20bc181527131d57a2b846134fe8f144a
= <=>
2021-06-03 22:51:45 +0530

    new commit update

---
+++ lib/sub/intro.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Homepage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: SafeArea(
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.start,
+         crossAxisAlignment: CrossAxisAlignment.center,
+        children:[
+          Text('data'),
+          Text('data'),
+        ],
+       )));
+  }
+}
